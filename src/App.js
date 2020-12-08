b5052dd8b0f8fa950009ab3c10caaf7463b85308
Victoria Goulko <vgoulko@seek.com.au>
2020-12-08 23:10:48 +1100

    Adding background and updating font

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends React.Component {
 		monster.name.toLowerCase().includes(searchField.toLowerCase()))
 		return (
 			<div className="App">
+				<h1> Monsters Rolodex </h1>
 				<SearchBox
 					placeholder='search monsters'
 					handleChange={(e) => this.handleChange(e)}
